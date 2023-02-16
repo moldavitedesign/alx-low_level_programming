05b9b1c58d599fe3d033a954371371bdf15011aa
Caleb <popoolacaleb@outlook.com>
2023-02-16 10:27:03 -0800

    please betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main - a program that prints exact output
+ * main - prints a string and standard error
  * Return: 1
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
