d40c585dad2b49ea94ddc016e26b5a3c8f2438da
Caleb <popoolacaleb@outlook.com>
2023-02-16 09:53:30 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Using the printf function to create a program
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
