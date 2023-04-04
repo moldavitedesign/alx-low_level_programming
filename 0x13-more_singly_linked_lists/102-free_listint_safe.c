474a4bdc7eafbd2d62966e6181550104b593e683
Caleb <popoolacaleb@outlook.com>
2023-04-04 15:27:41 -0700

    0x13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,97 +1,30 @@
 #include "lists.h"
 
-size_t count_nodes_in_loop(listint_t *head);
-size_t safe_free_listint(listint_t **h);
-
 /**
- * count_nodes_in_loop - Counts the number of unique nodes
- *                      in a looped listint_t linked list.
- * @head: A pointer to the head of the listint_t to check.
+ * free_listint_safe - Frees a listint_t list safely
+ * @h: pointer to the head of the list
  *
- * Return: If the list is not looped - 0.
- *         Otherwise - the number of unique nodes in the list.
+ * Return: size of the list that was free'd
  */
-size_t count_nodes_in_loop(listint_t *head)
+size_t free_listint_safe(listint_t **h)
 {
-    listint_t *tortoise, *hare;
-    size_t nodes = 1;
+	size_t len = 0;
+	listint_t *current, *temp;
 
-    if (head == NULL || head->next == NULL)
-        return (0);
+	if (h == NULL)
+		return (0);
 
-    tortoise = head->next;
-    hare = (head->next)->next;
+	current = *h;
+	while (current != NULL)
+	{
+		len++;
+		temp = current;
+		current = current->next;
+		free(temp);
+		if (temp <= current)
+			break;
+	}
+	*h = NULL;
 
-    while (hare)
-    {
-        if (tortoise == hare)
-        {
-            tortoise = head;
-            while (tortoise != hare)
-            {
-                nodes++;
-                tortoise = tortoise->next;
-                hare = hare->next;
-            }
-
-            tortoise = tortoise->next;
-            while (tortoise != hare)
-            {
-                nodes++;
-                tortoise = tortoise->next;
-            }
-
-            return (nodes);
-        }
-
-        tortoise = tortoise->next;
-        hare = (hare->next)->next;
-    }
-
-    return (0);
+	return (len);
 }
-
-/**
- * safe_free_listint - Frees a listint_t list safely (ie.
- *                     can free lists containing loops)
- * @h: A pointer to the address of
- *     the head of the listint_t list.
- *
- * Return: The size of the list that was freed.
- *
- * Description: The function sets the head to NULL.
- */
-size_t safe_free_listint(listint_t **h)
-{
-    listint_t *tmp;
-    size_t nodes, index;
-
-    nodes = count_nodes_in_loop(*h);
-
-    if (nodes == 0)
-    {
-        for (; h != NULL && *h != NULL; nodes++)
-        {
-            tmp = (*h)->next;
-            free(*h);
-            *h = tmp;
-        }
-    }
-
-    else
-    {
-        for (index = 0; index < nodes; index++)
-        {
-            tmp = (*h)->next;
-            free(*h);
-            *h = tmp;
-        }
-
-        *h = NULL;
-    }
-
-    h = NULL;
-
-    return (nodes);
-}
-
