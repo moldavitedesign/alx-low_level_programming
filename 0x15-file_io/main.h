e9066b0a2d494a3f305ef884aa1ae0696d3ad6a2
Caleb <popoolacaleb@outlook.com>
2023-04-09 13:03:36 -0700

    0x15. C - File I/O

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,20 +1,19 @@
 #ifndef MAIN_H
-#define MAIN_N
+#define MAIN_H
 
 /*
  * File: holberton.h
+ * Auth: Godswill Kalu
  * Desc: Header file containing prototypes for all functions
  *       written in the 0x14-file_io directory.
  */
 
-
-#include <elf.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <elf.h>
+#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
@@ -33,4 +32,5 @@ void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
+
 #endif /* MAIN_H */
