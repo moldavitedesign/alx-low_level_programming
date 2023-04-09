59c729099e9481835233d843df91f21274e92d0b
Caleb <popoolacaleb@outlook.com>
2023-04-09 13:08:53 -0700

    0x15. C - File I/O

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,13 +3,51 @@
 
 /*
  * File: holberton.h
+ * Auth: Godswill Kalu
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x14-file_io directory.
  */
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif /* MAIN_H */#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: holberton.h
+ * Auth: Godswill Kalu
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x14-file_io directory.
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif /* MAIN_H */
+
+#ifndef MAIN_H
+#define MAIN_N
+
 #include <elf.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
@@ -28,5 +66,4 @@ void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
-
-#endif /* MAIN_H */
+#endif
