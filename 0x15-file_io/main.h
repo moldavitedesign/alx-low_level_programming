ce178048b6ac853d9a20c29b959a3804780553d1
Caleb <popoolacaleb@outlook.com>
2023-04-09 12:27:39 -0700

    0x15. C - File I/O

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,17 +1,17 @@
 #ifndef MAIN_H
-#define MAIN_H
+#define MAIN_N
 
-#include <elf.hi>
+#include <elf.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
 
-extern ssize_t read_textfile(const char *filename, size_t letters);
-extern int create_file(const char *filename, char *text_content);
-extern int append_text_to_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 /**
  * ELF header functions
@@ -26,5 +26,4 @@ void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
-
 #endif
