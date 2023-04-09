22cc9339763a93b09467ef9feee518d221f7226b
Caleb <popoolacaleb@outlook.com>
2023-04-09 12:02:05 -0700

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,26 +1,26 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_binary - prints the binary representation of a number
- * @n: the number to print
+ * @n: parameter
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 0x8000000000000000;
-	int leading_zeroes = 1;
+	int i, count = 0;
+	unsigned long int current;
 
-	while (mask > 0) {
-		if (n & mask)
-			leading_zeroes = 0;
+	for (i = 63; i >= 0; i--)
+	{
+		current = n >> i;
 
-		if (!leading_zeroes)
-			printf("%lu", (n & mask) ? 1 : 0);
-
-		mask >>= 1;
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
 	}
-
-	if (leading_zeroes)
-		printf("0");
+	if (!count)
+		_putchar('0');
 }
-
