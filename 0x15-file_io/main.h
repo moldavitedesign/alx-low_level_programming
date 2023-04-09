319fd114bc26d417b8431d9e5e37b52c6ee26151
Caleb <popoolacaleb@outlook.com>
2023-04-09 12:19:05 -0700

    main file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 extern ssize_t read_textfile(const char *filename, size_t letters);
 extern int create_file(const char *filename, char *text_content);
 extern int append_text_to_file(const char *filename, char *text_content);
+
+
 #endif
